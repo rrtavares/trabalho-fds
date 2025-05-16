b47963e49ce4627ba971f34b59afa0ed45c00b0e
rtavares <31519857+rrtavares@users.noreply.github.com>
2025-05-16 11:28:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let botao = document.querySelector("#boasVindas")
+const mensagem = document.querySelector("#mensagem")
+
+let clicado=false;
+let contaClicks=0;
+
+botao.addEventListener("click", e =>{
+    contaClicks++;
+    if(contaClicks===1) 
+        mensagem.innerHTML = "Tenha compaixão, ajude os seus companheiros em qualquer oportunidade. Se a oportunidade não surge, saia do seu caminho para encontrá-la.";
+    if(contaClicks===2) 
+        mensagem.innerHTML = "O caminho do valente não segue os passos da estupidez.";
+    if(contaClicks===3) 
+        mensagem.innerHTML = "Enquanto se possa mexer, treine o corpo. Enquanto não se possa mexer, treine a mente.";
+    if(contaClicks===4) {
+        mensagem.innerHTML = "A perfeição é uma montanha impossível de escalar que deve ser escalada um pouco a cada dia.";
+        contaClicks = 0;
+    }
+
+})
+
+
+
+
+function message(){
+    
+}
